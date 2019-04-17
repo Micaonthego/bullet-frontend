f0bfb93704b1d177dde644c23555a12fd5089d64
github username <github email address>
2019-04-17 17:01:26 -0400

    CRUD done

--- index.js
+++ index.js
@@ -1,58 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM Content Loaded :)')
   const bulletDiv = document.querySelector('#bullet-div')
-  const form = document.querySelector('#create-bullet')
+  const form = document.querySelector('#bullet-form')
   const prioritiesInputTag = document.querySelector('#priorities')
   const gratitudeInputTag = document.querySelector('#gratitude')
   const accomplishmentsInputTag = document.querySelector('#accomplishments')
-  const improvementsInputTag = document.querySelector('#accomplishments')
-  let url = 'http://localhost:3000/api/bullets'
+  const improvementsInputTag = document.querySelector('#improvements')
+  const editPrioritiesInputTag = document.querySelector('#edit-priorities')
+  const editGratitudeInputTag = document.querySelector('#edit-gratitude')
+  const editAccomplishmentsInputTag = document.querySelector('#edit-accomplishments')
+  const editImprovementsInputTag = document.querySelector('#edit-improvements')
+  const img_urlInputTag = document.querySelector('#change-photo')
+  let url = "http://localhost:3000/api/bullets"
+  const imageForm = document.querySelector('#change-image')
+  const editForm = document.querySelector('#edit-form')
   let bullets;
-
-  // get the card that's clicked
-  // these are the props for my form that i need to copy:
-  // const image = card.querySelector('img').src
-  // const date = card.querySelector('span.card-title').innerText
-
-  // or if you have local state
-  // id = card.dataset.id
-  // bullet = bullets.find(id)
-  // image = bullet.image
-  // date = bullet.date
+  let bulletId;
 
 
   fetch(url)
     .then(resp => resp.json())
-    .then((function (json) {
-      bullets = json
+    .then((function (bullets) {
+      console.log('bullets', bullets)
       bullets.forEach(bullet => {
-        bulletDiv.innerHTML += `
-        <div class="row s6">
-        <div class="col s6">
-             <div class="card">
-      <div class="card-image waves-effect waves-block waves-light">
-        <img class="activator" src="${bullet.img_url}">
-      </div>
-      <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">${bullet.date}<i class="small material-icons right">arrow_upward</i></span>
-      </div>
-      <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">BULLET.<i class="material-icons right">close</i></span>
-        <h4>Day Log</h4>
-        <h6>Top Priorities</h6>
-        <p>${bullet.priorities}</p>
-        <h6>I am Grateful For</h6>
-        <p>${bullet.gratitude}</p>
-        <h4>Night Log</h4>
-        <h6>I Accomplished</h6>
-        <p>${bullet.accomplishments}</p>
-        <h6>I Will Improve</h6>
-        <p>${bullet.improvements}</p>
-      </div>
-    </div>
-    </div>
-    </div>
-            `
+        return renderBullets(bullet)
       })
 
     }))
@@ -60,54 +31,197 @@ document.addEventListener('DOMContentLoaded', () => {
 
   form.addEventListener('submit', e => {
     e.preventDefault()
-    console.log(e.target.dataset.action === 'save-bullet')
-    if (e.target.dataset.action === 'save-bullet') {
-      const priorities = prioritiesInputTag.vaue
-      const gratitude = gratitudeInputTag.value
-      const accomplishments = accomplishmentsInputTag.value
-      const improvements = improvementsInputTag.value
+    console.log(e.target)
+    const prioritiesInput = prioritiesInputTag.value
+    const gratitudeInput = gratitudeInputTag.value
+    const accomplishmentsInput = accomplishmentsInputTag.value
+    const improvementsInput = improvementsInputTag.value
 
-      fetch(url, {
-          method: 'POST',
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          priorities: prioritiesInput,
+          gratitude: gratitudeInput,
+          accomplishments: accomplishmentsInput,
+          improvements: improvementsInput
+        })
+      })
+      .then(res => res.json())
+      .then(function (bullet) {
+        return renderBullets(bullet)
+      })
+    form.reset()
+  })
+
+  bulletDiv.addEventListener('click', e => {
+    bulletId = parseInt(e.target.dataset.id)
+    if (e.target.dataset.action === 'edit') {
+      // console.log(bulletId)
+      // console.log(e.target.parentNode)
+      // console.log(e.target.parentNode.querySelector('#edit-form'))
+      // console.log(editForm)
+      editForm.setAttribute("style", "visible")
+      // editForm.style.visibility = "visible"
+      fetch(`${url}/${bulletId}`)
+        .then(res => res.json())
+        .then(bullets => {
+          console.log(bullets)
+          const editPrioritiesInput = editPrioritiesInputTag
+          const editGratitudeInput = editGratitudeInputTag
+          const editAccomplishmentsInput = editAccomplishmentsInputTag
+          const editImprovementsInput = editImprovementsInputTag
+
+          editPrioritiesInput.value = bullets.priorities
+          editGratitudeInput.value = bullets.gratitude
+          editAccomplishmentsInput.value = bullets.accomplishments
+          editImprovementsInput.value = bullets.improvements
+        })
+    }
+  })
+
+  editForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const editBulletBtn = e.target.querySelector('#edit-bullet')
+    if (editBulletBtn.dataset.action === 'edit-bullet') {
+      const editPrioritiesInput = editPrioritiesInputTag
+      const editGratitudeInput = editGratitudeInputTag
+      const editAccomplishmentsInput = editAccomplishmentsInputTag
+      const editImprovementsInput = editImprovementsInputTag
+      const newPrioritiesInput = editPrioritiesInput.value
+      const newGratitudeInput = editGratitudeInput.value
+      const newAccomplishmentsInput = editAccomplishmentsInput.value
+      const newImprovementsInput = editImprovementsInput.value
+
+      fetch(`${url}/${bulletId}`, {
+          method: 'PATCH',
           headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
           },
           body: JSON.stringify({
-            priorities: (priorities),
-            gratitude: (gratitude),
-            accomplishments: (accomplishments),
-            improvements: (improvements)
+            priorities: newPrioritiesInput,
+            gratitude: newGratitudeInput,
+            accomplishments: newAccomplishmentsInput,
+            improvements: newImprovementsInput
           })
         })
         .then(res => res.json())
-        .then(function (newBullet) {
-          return bulletDiv.innerHTML += `
-        <div class="card">
-      <div class="card-image waves-effect waves-block waves-light">
-        <img class="activator" src="${newBullet.img_url}">
-      </div>
-      <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">${newBullet.date}<i class="small material-icons right">arrow_upward</i></span>
-      </div>
-      <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">BULLET.<i class="material-icons right">close</i></span>
-        <h4>Day Log</h4>
-        <h6>Top Priorities</h6>
-        <p>${newBullet.priorities}</p>
-        <h6>I am Grateful For</h6>
-        <p>${newBullet.gratitude}</p>
-        <h4>Night Log</h4>
-        <h6>I Accomplished</h6>
-        <p>${newBullet.accomplishments}</p>
-        <h6>I Will Improve</h6>
-        <p>${newBullet.improvements}</p>
+        .then(bullet => {
+          const pri = document.querySelector(`#pri-${bullet.id}`)
+          const gra = document.querySelector(`#gra-${bullet.id}`)
+          const acp = document.querySelector(`#acp-${bullet.id}`)
+          const imp = document.querySelector(`#imp-${bullet.id}`)
+          pri.innerText = bullet.priorities
+          gra.innerText = bullet.gratitude
+          acp.innerText = bullet.accomplishments
+          imp.innerText = bullet.improvements
+        })
+      editForm.reset()
+      editForm.style.visibility = "hidden"
+    }
+  })
+
+
+  bulletDiv.addEventListener('click', e => {
+    bulletId = parseInt(e.target.dataset.id)
+    if (e.target.dataset.action === 'add-photo') {
+      // console.log('clicked')
+      // console.log(bulletId)
+      imageForm.style.visibility = "visible"
+    }
+  })
+
+  imageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log(e.target)
+    const savePhotoBtn = e.target.querySelector('#save-photo')
+    if (savePhotoBtn.dataset.action === 'save-photo') {
+      const img_urlInput = img_urlInputTag.value
+
+      console.log(img_urlInput)
+      fetch(`${url}/${bulletId}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify({
+            img_url: img_urlInput
+          })
+        })
+        .then(res => res.json())
+        .then(bullet => {
+          const image = document.querySelector(`#image[data-id="${bulletId}"]`)
+          console.log(image)
+          image.src = bullet.img_url
+        })
+      imageForm.reset()
+      imageForm.style.visibility = "hidden"
+    }
+  })
+
+  bulletDiv.addEventListener('click', e => {
+    bulletId = parseInt(e.target.dataset.id)
+    if (e.target.dataset.action === 'delete') {
+      // console.log('clicked')
+      // console.log(bulletId)
+      fetch(`${url}/${bulletId}`, {
+          method: 'delete'
+        })
+        .then(res => res.json())
+        .then(bulletDeleted => {
+          const deletedCard = document.querySelector(`#card-${bulletDeleted.id}`)
+          deletedCard.remove()
+        })
+
+    }
+  })
+
+  function renderBullets(bullet) {
+    return bulletDiv.innerHTML += `
+        <div class="row">
+        <div class="col s4">
+             <div id="card-${bullet.id}" class="card">
+        <div class="card-image waves-effect waves-block waves-light">
+        <img id='image' data-id=${bullet.id} src="${bullet.img_url}">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">${bullet.date}<i class="small material-icons right">arrow_upward</i></span><i data-id=${bullet.id} data-action='add-photo' class="tiny material-icons left">add_a_photo</i>
+        </div>
+        <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+        <i id='delete' data-id=${bullet.id} data-action='delete' class="tiny material-icons right">delete</i><i data-id=${bullet.id} data-action='edit' class="tiny material-icons right">edit</i>
+        <i class = "material-icons prefix tiny"> brightness_low</i>
+        <p>Top Priorities</p>
+        <p id="pri-${bullet.id}">${bullet.priorities}</p>
+        <p>I am Grateful For</p>
+        <p id="gra-${bullet.id}">${bullet.gratitude}</p>
+        <i class="material-icons prefix tiny">brightness_2</i>
+        <p>I Accomplished</p>
+        <p id="acp-${bullet.id}">${bullet.accomplishments}</p>
+        <p>I Will Improve</p>
+        <p id="imp-${bullet.id}">${bullet.improvements}</p>
       </div>
     </div>
-        `
-        })
-    }
-    form.reset()
-  })
+    </div>
+    </div>
+            `
+  }
+  // Add a button to submit the form
+  // append the bulletId to the form tor the button (somewhere in the DOM)
+  // PATCH localhost:3000/bullets/:id ()
+
+  // after the form is submitted, toggle visibility back to hidden
+
+
+
+  // 
+
+
+
 
 })
